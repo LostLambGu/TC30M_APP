0742674f1cb1b580ce47c83b66deb0a932737c6a
Yangjie Gu <guyjwork@foxmail.com>
2017-12-08 10:23:36 +0800

    add files

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TC30MApplication' 
+ * Target:  'TC30MApplication' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
